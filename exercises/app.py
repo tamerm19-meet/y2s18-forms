ed21f5279ddb5e23c2702d19a1d487732c59ba84
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 18:30:04 +0300

    aya: neow neow neow neow

--- exercises/app.py
+++ exercises/app.py
@@ -10,4 +10,17 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
+@app.route('/add', methods = ['GET', 'POST'])
+def add_student_route():
+	if resquest.method == 'GET':
+		return render_template('add.html')
+	else:
+		student_name = request.form['student_name']
+		student_year = request.form['student_year']
+
+		add_student(student_name, student_year, False)
+
+		return render_template('add.html', student_name = student_name, student_year = student_year)
+		# print("Recieved a POST request")
+		# return render_template('add.html')
 app.run(debug=True)
